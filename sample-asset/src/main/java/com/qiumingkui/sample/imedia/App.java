e1fb41005d6f9faddd8cfd86021252acf04fd593
邱明奎 <qmkemail@sina.com>
2017-03-25 13:25:49 +0800

    新建 simple-asset module

---
+++ sample-asset/src/main/java/com/qiumingkui/sample/imedia/App.java
@@ -0,0 +1,13 @@
+package com.qiumingkui.sample.imedia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
