7f974823bda4aca1ed730e22f2480a5e7c95d6e2
邱哥 <qmkemail@sina.com>
2017-03-28 09:04:35 +0800

    迁移 makemoney 到 mp

--- sample-mp/src/main/java/com/qiumingkui/sample/imedia/Application.java
+++ sample-mp/src/main/java/com/qiumingkui/sample/imedia/Application.java
@@ -1,12 +1,13 @@
 package com.qiumingkui.sample.imedia;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
